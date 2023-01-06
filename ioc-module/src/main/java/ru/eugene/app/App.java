855556b788fbac2d2ad7254b9799bde6ad36e733
forprog2008 <forprog2008@gmail.com>
2023-01-06 11:55:32 +0400

    Added lookup and output

--- ioc-module/src/main/java/ru/eugene/app/App.java
+++ ioc-module/src/main/java/ru/eugene/app/App.java
@@ -2,6 +2,7 @@ package ru.eugene.app;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.eugene.app.service.IService;
 
 public class App {
     public static void main(String[] args) {
@@ -10,5 +11,9 @@ public class App {
         for (String definitionName : definitionNames) {
             System.out.println(definitionName);
         }
+
+        IService iService = context.getBean(IService.class);
+        String daoString = iService.interactWithDaoString();
+        System.out.println(daoString);
     }
 }
